09cc722ece090c7f3ec6d87fe9715f9186360f4c
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-09-20 11:48:21 +0200

    aggiunta descrizione Observer

--- Observer.h
+++ Observer.h
@@ -9,8 +9,11 @@
 
 class Observer {
 public:
+    //distruttore della classe dichiarato come virtuale per poter gestire correttamente la memoria
     virtual ~Observer() = default;
 
+    //metodo che viene sovrascritto nelle classi derivate che serve a notificare un aggiornamento
+    //della lista specificata
     virtual void onUpdate(const std::string& listName) = 0;
 };
 
