8c23278d127719e43b3050fc261c67ab3c5dea62
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-09-20 12:13:22 +0200

    aggiunta descrizione ShoppingList

--- ShoppingList.h
+++ ShoppingList.h
@@ -19,50 +19,56 @@
 
 class ShoppingList: public Subject {
 public:
-    // Costruttore della classe ShoppingList
-    explicit ShoppingList(const std::string &name) : listName(name) {}
+    //Costruttore della classe ShoppingList che inizializza il nome della lista
+    explicit ShoppingList(std::string name) : listName(std::move(name)) {}
 
-    // Metodi per l'implementazione dell'Observer pattern
+    //implementazione del pattern Observer: aggiunge un osservatore
     void addObserver(Observer* observer) override;
+
+    //implementazione del pattern Observer: rimuove un osservatore
     void removeObserver(Observer* observer) override;
+
+    //implementazione del pattern Observer: notifica tutti gli osservatori
     void notifyObservers() override;
 
-    // Aggiunge un oggetto alla lista della spesa
+    //aggiunge un nuovo articolo alla lista della spesa
     void insertItem(const Item& item);
 
-    // Rimuove un oggetto dalla lista della spesa
+    //rimuove un articolo dalla lista della spesa ricercandolo per nome
     void removeItem(const std::string& itemName);
 
-    // Imposta uno stato di acquisto per un oggetto nella lista
+    //segna l'articolo come acquistato, ricercando l'articolo per nome
     void markItemAsBought(const std::string& itemName);
 
-    // Restituisce il numero di oggetti non acquistati nella lista
+    //restituisce il numero di articoli non ancora acquistati nella lista
     int markItemsAsNotBought() const;
 
-    // Stampa gli oggetti non acquistati nella lista
+    //stampa gli articoli non acquistati nella lista
     void printNotBoughtItems() const;
 
-    // Restituisce il nome della lista della spesa
+    //restituisce il nome della lista della spesa
     const std::string& getListName() const;
 
-    // Imposta il nome della lista della spesa
+    //imposta il nome della lista della spesa
     void setListName(const std::string& name);
 
-    // Restituisce la mappa delle categorie degli oggetti
+    // Restituisce una mappa che associa le categorie degli articoli alla loro quantità
     const std::map<std::string, int>& getItemCategories() const;
 
+    // Restituisce il numero totale di articoli nella lista della spesa
     int getListSize() const;
 
+    // Trova e restituisce un articolo in base al nome, se esiste
     const std::shared_ptr<Item> findItem(const std::string& name) const;
 
-    // Distruttore virtuale della classe ShoppingList
-    virtual ~ShoppingList() = default;
+    //distruttore della classe dichiarato come virtuale per poter gestire correttamente la memoria
+    ~ShoppingList() override = default;
 
 private:
-    std::list<Observer*> observers;                  // Lista degli osservatori
-    std::map<std::string, std::shared_ptr<Item>> items; // Mappa degli oggetti nella lista
-    std::map<std::string, int> itemCategories;                // Mappa delle categorie degli oggetti
-    std::string listName;                    // Nome della lista della spesa
+    std::list<Observer*> observers; //elenco degli osservatori registrati
+    std::map<std::string, std::shared_ptr<Item>> items; //mappa che contiene gli articoli nella lista, indicizzati per nome
+    std::map<std::string, int> itemCategories; //mappa delle categorie degli articoli con il conteggio degli articoli per categoria
+    std::string listName; //nome della lista
 };
 
 
