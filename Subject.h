5a62be9e4a3a63249767d548722c7ce31c0d5ee7
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2024-09-20 11:52:35 +0200

    aggiunta descrizione Subject

--- Subject.h
+++ Subject.h
@@ -9,12 +9,16 @@
 
 class Subject {
 public:
+    //distruttore della classe dichiarato come virtuale per poter gestire correttamente la memoria
     virtual ~Subject() = default;
 
+    //aggiunge un osservatore alla lista degli osservatori
     virtual void addObserver(Observer* observer) = 0;
 
+    //rimuove un osservatore dalla lista degli osservatori
     virtual void removeObserver(Observer* observer) = 0;
 
+    //notifica tutti gli osservatori registrati di un cambiamento
     virtual void notifyObservers() = 0;
 };
 
