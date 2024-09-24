5b171bac2978fd8123798afcb8fa8066657a7d7f
pinguinoadelia <103119088+pinguinoadelia@users.noreply.github.com>
2024-09-24 14:38:48 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/Test/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/michelino/Documents/LabProgrammazione/Test/lib/googletest/googlemock
-# Build directory: /Users/michelino/Documents/LabProgrammazione/cmake-build-debug/Test/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/Test/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/michelino/Documents/LabProgrammazione/Test/lib/googletest/googletest
-# Build directory: /Users/michelino/Documents/LabProgrammazione/cmake-build-debug/Test/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
